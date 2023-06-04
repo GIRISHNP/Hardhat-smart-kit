c70e75f34bd337d80a94163fb58bbe204a944cfd
Girish <parameshnallur65@gmail.com>
2023-06-04 13:31:57 +0530

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox")
-require("./tasks")
+require("@nomicfoundation/hardhat-chai-matchers")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
 require("dotenv").config()
 
 const COMPILER_SETTINGS = {
@@ -12,25 +17,25 @@ const COMPILER_SETTINGS = {
     },
 }
 
-const MAINNET_RPC_URL =
-    process.env.MAINNET_RPC_URL ||
-    process.env.ALCHEMY_MAINNET_RPC_URL ||
-    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const POLYGON_MAINNET_RPC_URL =
-    process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+// const MAINNET_RPC_URL =
+//     process.env.MAINNET_RPC_URL ||
+//     process.env.ALCHEMY_MAINNET_RPC_URL ||
+//     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+// const POLYGON_MAINNET_RPC_URL =
+//     process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
 const SEPOLIA_RPC_URL =
     process.env.SEPOLIA_RPC_URL;
-const MUMBAI_RPC_URL =
-    process.env.MUMBAI_RPC_URL || "https://polygon-mumbai.g.alchemy.com/v2/your-api-key"
+// const MUMBAI_RPC_URL =
+//     process.env.MUMBAI_RPC_URL || "https://polygon-mumbai.g.alchemy.com/v2/your-api-key"
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 // optional
-const MNEMONIC = process.env.MNEMONIC || "Your mnemonic"
-const FORKING_BLOCK_NUMBER = parseInt(process.env.FORKING_BLOCK_NUMBER) || 0
+// const MNEMONIC = process.env.MNEMONIC || "Your mnemonic"
+// const FORKING_BLOCK_NUMBER = parseInt(process.env.FORKING_BLOCK_NUMBER) || 0
 
 // Your API key for Etherscan, obtain one at https://etherscan.io/
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
-const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
-const REPORT_GAS = process.env.REPORT_GAS || false
+// const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+// const REPORT_GAS = process.env.REPORT_GAS || false
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -51,16 +56,7 @@ module.exports = {
         ],
     },
     networks: {
-        hardhat: {
-            hardfork: "merge",
-            // If you want to do some forking set `enabled` to true
-            forking: {
-                url: MAINNET_RPC_URL,
-                blockNumber: FORKING_BLOCK_NUMBER,
-                enabled: false,
-            },
-            chainId: 31337,
-        },
+
         localhost: {
             chainId: 31337,
         },
@@ -72,24 +68,24 @@ module.exports = {
             //   },
             chainId: 11155111,
         },
-        mainnet: {
-            url: MAINNET_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            //   accounts: {
-            //     mnemonic: MNEMONIC,
-            //   },
-            chainId: 1,
-        },
-        polygon: {
-            url: POLYGON_MAINNET_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            chainId: 137,
-        },
-        mumbai: {
-            url: MUMBAI_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            chainId: 80001,
-        },
+        // mainnet: {
+        //     url: MAINNET_RPC_URL,
+        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        //     //   accounts: {
+        //     //     mnemonic: MNEMONIC,
+        //     //   },
+        //     chainId: 1,
+        // },
+        // polygon: {
+        //     url: POLYGON_MAINNET_RPC_URL,
+        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        //     chainId: 137,
+        // },
+        // mumbai: {
+        //     url: MUMBAI_RPC_URL,
+        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        //     chainId: 80001,
+        // },
     },
     defaultNetwork: "hardhat",
     etherscan: {
@@ -97,13 +93,13 @@ module.exports = {
         apiKey: {
             // npx hardhat verify --list-networks
             sepolia: ETHERSCAN_API_KEY,
-            mainnet: ETHERSCAN_API_KEY,
-            polygon: POLYGONSCAN_API_KEY,
-            polygonMumbai: POLYGONSCAN_API_KEY,
+            // mainnet: ETHERSCAN_API_KEY,
+            // polygon: POLYGONSCAN_API_KEY,
+            // polygonMumbai: POLYGONSCAN_API_KEY,
         },
     },
     gasReporter: {
-        enabled: REPORT_GAS,
+        enabled: true,
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
